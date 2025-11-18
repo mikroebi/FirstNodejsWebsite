ab3dad0d6058a4ec7da88fd8fc3a2944b432d8a9
unknown <bmikroebi@gmail.com>
2025-11-18 19:53:17 +0330

    Edit Deploy.yml

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // این خط را اضافه کنید
   output: 'export', 
-  // و این خط را هم اضافه کنید تا مسیرها به درستی کار کنند
+  
   trailingSlash: true,
+  
+
   images: {
-    // برای تصاویر محلی مشکلی نیست اما برای تصاویر خارجی باید loader سفارشی ساخت
-    // در حالت استاتیک، بهتر است از تصاویر محلی استفاده کنید
-    unoptimized: true, 
+    unoptimized: true,
   },
+  
+
 };
 
 export default nextConfig;
